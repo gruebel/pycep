591b04f71b2e52d7dfc14acb708a2e1fc2c89cf7
kapyteinaikido <166383531+kapyteinaikido@users.noreply.github.com>
2025-09-04 23:31:27 +0200

    Support loop and conditional blocks in child resources (#297)
    * Support loop and conditional blocks in child resources
    
    * Add tests
    
    * Formatting
    
    ---------
    
    Co-authored-by: nadiraikido <166383531+nadiraikido@users.noreply.github.com>

--- tests/test_parse_loop.py
+++ tests/test_parse_loop.py
@@ -48,6 +48,19 @@ def test_parse_loop_object() -> None:
     assert_that(result).is_equal_to(expected_result)
 
 
+def test_parse_loop_child_resources() -> None:
+    # given
+    sub_dir_path = EXAMPLES_DIR / "child"
+    file_path = sub_dir_path / "main.bicep"
+    expected_result = json.loads((sub_dir_path / "result.json").read_text())
+
+    # when
+    result = BICEP_PARSER.parse(file_path=file_path)
+
+    # then
+    assert_that(result).is_equal_to(expected_result)
+
+
 def test_parse_loop_condition() -> None:
     # given
     sub_dir_path = EXAMPLES_DIR / "condition"
